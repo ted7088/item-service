dc05c8f0916213b067989ead21acfb2e68e4f1bc
master <ted3131@diquest.com>
2023-11-21 16:48:51 +0900

    commit

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,9 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -36,6 +34,26 @@ public class BasicItemController {
     public String item(@PathVariable long itemId, Model model){
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item",item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm(){
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save(@RequestParam String itemName, @RequestParam int price, @RequestParam Integer quantity , Model model){
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item",item);
+
         return "basic/item";
     }
 
